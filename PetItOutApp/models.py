838b60b8c21ce9a24803196e7b93bc4fd241cf1d
Haoran Liu <2785456L@student.gla.ac.uk>
2023-03-16 03:31:14 +0000

    userprofile model and form finsished, final adjustment for template in progress

--- PetItOutApp/models.py
+++ PetItOutApp/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.template.defaultfilters import slugify
 from django.contrib.auth.models import User
+from django.db import models
+
+
+
 
 
 # Create your models here.
@@ -8,8 +12,27 @@ class UserProfile(models.Model):
     # This line is required. Links UserProfile to a User model instance.
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     # The additional attributes we wish to include.
-    website = models.URLField(blank=True)
     picture = models.ImageField(upload_to='profile_images', blank=True)
+    objects = models.Manager()
 
     def __str__(self):
         return self.user.username
+    
+class EditProfile(models.Model):
+    user_description = models.TextField()
+    user = models.ForeignKey("UserProfile",on_delete=models.CASCADE)
+    def __str__(self):
+        return self.user_description
+
+class PetProfile(models.Model):
+    pet_name = models.TextField(blank=True,null=True)
+    pet_type = models.TextField(blank=True,null=True)
+    pet_age = models.TextField(blank=True,null=True)
+    pet_description = models.TextField(blank=True,null=True)
+    pet_picture = models.ImageField(upload_to='pet_images',blank=True,null=True)
+    objects = models.Manager()
+    def __str__(self):
+        return self.pet_name
+
+
+
