af6ee6a8618c5e0b0e35e087655f8c085978d339
Haoran Liu <2785456L@student.gla.ac.uk>
2023-03-19 20:24:31 +0000

    changed model.py

--- PetItOutApp/models.py
+++ PetItOutApp/models.py
@@ -11,7 +11,7 @@ from django.core.exceptions import FieldDoesNotExist
 # Create your models here.
 # Create A User Profile Model
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE,primary_key=True)
     picture = models.ImageField(null=True, blank=True, upload_to="user_profile_images")    
     user_description = models.TextField(blank=True,null=True)
 
@@ -19,17 +19,9 @@ class UserProfile(models.Model):
     def __str__(self):
         return self.user.username
 
-# Create Profile When New User Signs Up
-#@receiver(post_save, sender=User)
-# def create_profile(sender, instance, created, **kwargs):
-# 	if created:
-# 		user_profile = UserProfile(user=instance)
-# 		user_profile.save()
-
-# post_save.connect(create_profile, sender=User)
 
 
-
+# A PetProfile with likes field, where user can like a pet, but only once to the same pet
 class PetProfile(models.Model):
     userprofile = models.OneToOneField(UserProfile,on_delete=models.CASCADE)
     pet_name = models.TextField(blank=True,null=True)
@@ -45,20 +37,12 @@ class PetProfile(models.Model):
     
     def __str__(self):
         return self.pet_name
-    
+
+# map two pet profile to a battle
 class Battle(models.Model):
     petprofileRed = models.ForeignKey(PetProfile,related_name="petprofileRed",on_delete=models.CASCADE)
     petprofileBlue = models.ForeignKey(PetProfile,related_name="petprofileBlue",on_delete=models.CASCADE)
 
     def __str__(self):
-        # try:
-        #     field = self._meta.get_field(self.petprofileRed)
-        #     field2 = self._meta.get_field(self.petprofileBlue)
-        #     def randomword(length):
-        #         letters = string.ascii_lowercase
-        #         return ''.join(random.choice(letters) for i in range(length))
-        #     returnString = self.petprofileRed.userprofile.user.username + randomword(5)
-        # except FieldDoesNotExist:
-            
         return self.petprofileRed.pet_name+self.petprofileBlue.pet_name
     
--- PetItOutApp/views.py
+++ PetItOutApp/views.py
@@ -12,58 +12,66 @@ from django.contrib.auth import authenticate, login, logout
 from PetItOutApp.models import UserProfile,PetProfile,Battle
 from django.contrib import messages
 from django.core.exceptions import ObjectDoesNotExist
-from PetItOutApp.bing_search import run_query
 import json
-from django.core.files import File
+from django.db.models import Count
 
+# this is the home page, which should include a list of battles, see template
 def home_page(request):
     battle_view = Battle.objects.all
     return render(request, 'PetItOut/home_page.html',context={'battle_views':battle_view})
-
+# this is the battle page, which can be redirected from home_page, includes pet information in the page
 def battle(request,username):
     username = username
     context_dict = {}
-    battle_view = Battle.objects.get(petprofileRed__userprofile__user__username=username)
+    i = 1;
+    try:
+        battle_view = Battle.objects.get(petprofileRed__userprofile__user__username=username)
+    except:
+        try:
+            battle_view = Battle.objects.get(petprofileRed__userprofile__user__username=username)[0]
+        except:
+            battle_view = Battle.objects.filter(petprofileRed__userprofile__user__username=username)[i]
+            i+=1;
 
     context_dict['battle_view'] = battle_view
+    # when user try to vote for the pet competitions
     if request.method=="POST":
-        if request.POST.get('operation') =="like_submit_red" and request.is_ajax():
-            likes_view = get_object_or_404(PetProfile,userprofile__user__username=username)
-            print(likes_view)
-            likes_view.likes.add(request.user)
-            context_dict['pet_profile']=likes_view
-            return HttpResponse(json.dumps(context_dict),likes_view__type='application/json')
-        elif request.POST.get('operation') =="like_submit_blue" and request.is_ajax():
-            likes_view = get_object_or_404(PetProfile,userprofile__user__username=username)
-            print(likes_view)
-            likes_view.likes.add(request.user)
-            context_dict['pet_profile']=likes_view
-            return HttpResponse(json.dumps(context_dict),likes_view__type='application/json')
+        # user must login to vote
+        if request.user.is_authenticated:
+            if request.POST.get('operation') =="like_submit_red" and request.is_ajax():
+                likes_view = get_object_or_404(PetProfile,userprofile__user__username=username)
+                print(likes_view)
+                likes_view.likes.add(request.user)
+                context_dict['pet_profile']=likes_view
+                return HttpResponse(json.dumps(context_dict),likes_view__type='application/json')
+            elif request.POST.get('operation') =="like_submit_blue" and request.is_ajax():
+                likes_view = get_object_or_404(PetProfile,userprofile__user__username=username)
+                print(likes_view)
+                likes_view.likes.add(request.user)
+                context_dict['pet_profile']=likes_view
+                return HttpResponse(json.dumps(context_dict),likes_view__type='application/json')
+        else:
+            return render(request,'PetItOut/battle.html',context=context_dict)
     likes_view=PetProfile.objects.all
     return render(request,'PetItOut/battle.html',context=context_dict)
 
+# this map a list of 10 pets in order of the number of likes they receive
 def hallOfFame(request):
-    pet_profiles = PetProfile.objects.order_by('-likes')[:10]
+    pet_profiles = PetProfile.objects.annotate(num_likes=Count('likes')).order_by('-num_likes')[:10]
     print(pet_profiles)
     return render(request,'PetItOut/hall_of_fame.html',context={'pet_profiles':pet_profiles})
 
+# search within databse of pets, pass back pet_profile, receive search input from template
 def search(request):
-    # result_list = []
-    # query = ''
-
     if request.method == 'POST':
         searched = request.POST['searched']
         pet_profiles = PetProfile.objects.filter(pet_name__contains=searched)
         print(pet_profiles)
-    #     query = request.POST['query'].strip()
-
-    #     if query:
-    #         result_list = run_query(query)   'result_list': result_list, 'query': query
-
         return render(request, 'PetItOut/search_result.html', context={'searched':searched,'pet_profiles':pet_profiles})
     else:
         return render(request, 'PetItOut/search_result.html', {})
 
+# a standard register field
 def register(request):
     registered = False
     if request.method == 'POST':
@@ -90,6 +98,7 @@ def register(request):
     
     return render(request, 'PetItOut/register.html', context = {'user_form': user_form, 'profile_form': profile_form, 'registered': registered})
 
+# this pass to templates a list of pets, if the database is empty, this would return back to home_page
 @login_required
 def profile_list(request):
     picturefound=False;
@@ -101,19 +110,19 @@ def profile_list(request):
         redirect(reverse('PetItOut:home_page'))
     return render(request,'PetItOut/profile_list.html',context={'pet_profiles':pet_profiles,'picturefound':picturefound})
 
+# 
 @login_required
 def edit_profile(request,username):
     username = request.user.username
-    try:
-        pet_form = PetProfileForm(request.POST or None, request.FILES or None)
-    except IntegrityError:
-        pet_form = PetProfileForm(request.POST or None, request.FILES or None)
+    pet_form = PetProfileForm(request.POST or None, request.FILES or None)
+    # just like register, user post a form back to databse, with pet_type, name and so on, refer back to form
     if pet_form.is_valid():
         try:
             pet_profile_user = PetProfile.objects.get(userprofile__user=request.user)
             pet_form = PetProfileForm(request.POST or None, request.FILES or None, instance=pet_profile_user)
             pet_form.save()
         except:
+            # if the users do not have petprofiles link to them, then petprofiles would be created instead of amending to existing ones
             profile = UserProfile.objects.get(user__username=username)
             PetProfile.objects.create(userprofile=profile,pet_name=pet_form.cleaned_data['pet_name'],pet_type=pet_form.cleaned_data['pet_type'],pet_age=pet_form.cleaned_data['pet_age'],pet_description=pet_form.cleaned_data['pet_description'],pet_picture=pet_form.cleaned_data['pet_picture'])
         redirect(reverse("PetItOut:user_profile" ,args=[username]))
@@ -121,6 +130,7 @@ def edit_profile(request,username):
         
 
 def user_login(request):
+    # Standard login interface, Profile image, username, password and email required
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
@@ -141,6 +151,9 @@ def user_login(request):
 
     
 @login_required
+# Two part in this page, first part is request.user, which is the current logged user's userprofile,
+# second part is the user other than the logged user
+# Either way, a username would be return and mapped to the url
 def user_profile(request,username):
     username = username
     if request.method=='POST':
@@ -148,6 +161,7 @@ def user_profile(request,username):
             pet_profile_red = PetProfile.objects.get(userprofile__user__username=username)
             pet_profile_blue = PetProfile.objects.get(userprofile__user__username=request.user.username)
             Battle.objects.create(petprofileRed=pet_profile_red,petprofileBlue=pet_profile_blue)
+# if the user does not have a petprofile link to them, then they can't challenge other users' pet
         except:
             messages.error(request,"You don't have a pet, or you already started a battle with it")
     print(username)
